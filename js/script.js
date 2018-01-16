9e901e982c82cab722909f35087bad70bfca296f
Paul Walker <pw97960@gmail.com>
2018-01-16 08:54:19 -0800

    Updated script.js file

--- js/script.js
+++ js/script.js
@@ -1,46 +1,95 @@
-// event listener to respond to "Show another quote" button clicks
-getRandomQuote() {
+// Define variable to hold the value of the array
+var makeRandomQuotes = 0;
 
-}
-// when user clicks anywhere on the button, the "printQuote" function is called
-printQuote() {
 
-}
-// Array of objects for the quotes
+// Assign quotes to an array of objects
 var quotes = [
-	{ 
-		quote: "No one ever does live happily ever after, but we leave the children to find that out for temselves.", 
-		source: "Stephen King", 
-		cite: "Wolves of the Calla"
-	},
-	{
-		quote: "Happiness is that state of consciousness which proceeds from the achievement of one's values.",
-		source: "Ayn Rand",
-		cite: "Atlas Shrugged"
-	},
-	{
-		quote: "Happiness. Simple as a glass of chocolate or tortuous as the heart. Bitter. Sweet. Alive.",
-		source: "Joanne Harris",
-		cite: "Chocolat"
-	},
-	{
-		quote: "Give a man health and a course to steer; and he'll never stop to trouble about whether he's happy or not.",
-		source: "George Bernard Shaw",
-		cite: "Captain Brassbound's Conversion"
-	},
-	{
-		quote: "What we need for our happiness is often close at hand, if we knew but how to seek for it.",
-		source: "Nathaniel Hawthorne",
-		cite: "American Note-Books",
-		date: "August 22, 1837"
-	},
-	{
-		quote: "I kept looking for happiness, and then I realized: This is it. It's a moment, and it comes, and it boes, and it'll come back again. I yearn for things, but at the same time I'm just peaceful.",
-		source: "Nicole Kidman",
-		cite: "Vanity Fair",
-		date: "October 2007"
-	}
+	
+			{ 	quote: 		"Badges? We ain't got no badges! We don't need no badges! I don't have to show you any stinkin' badges!",
+				source: 	"Alfonso Bedoya",
+				citation:   "The Treasure of the Sierra Madre",
+				year: 		1948,
+				tag:        "Movie: Action",   
+			},
+			{ 	quote: 		"Here's looking at you, kid.",
+				source: 	"Humphrey Bogart",
+				citation:   "Casablanca",
+				year: 		1942,
+				tag:        "Movie: Suspense"      
+			},
+			{ 	quote: 		"Fasten your seatbelts. It's going to be a bumpy night.",
+				source: 	"Bette Davis",
+				citation:   "All About Eve",
+				tag:		"Movie: Drama",   
+			},
+			{ 	quote: 		"There's no place like home.",
+				source: 	"Judy Garland",
+				citation:   "The Wizard of Oz",
+				year: 		1939,
+				tag:  		"Movie: Fantasy",   
+			},
+			{ 	quote: 		"I'll be back.",
+				source: 	"Arnold Schwarzenegger",
+				citation:   "The Terminator",
+				tag:        "Movie: Science Fiction",   
+			},
+			{ 	quote: 		"Life moves pretty fast. You don't stop and look around once in a while, you could miss it.",
+				source: 	"Matthew Broderick",
+				citation:   "Ferris Bueller",
+				year: 		1986,
+				tag:        "Movie: Comedy",   
+			}
 ];
+// event listener to respond to "Show another quote" button clicks
+function getRandomQuote() {
+	makeRandomQuotes = Math.floor( Math.random() * quotes.length );
+	var makeQuote = quotes[makeRandomQuotes];
+	return makeQuote;	
+}
 
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+function printQuote() { 
+	var printMakeQuote = getRandomQuote();
+	var getCitation = isThereCitation(printMakeQuote.citation);
+	var getYear = isThereYear(printMakeQuote.year);
+	console.log(getYear);
+	
+	function isThereYear(objectProp) {	
+		if ( objectProp !== undefined ) {
+			return '<span class="year">' + objectProp + '</span>';			
+		} else {
+			return '';
+		}		
+	}
+
+	function isThereCitation(objectProp) {	
+		if ( objectProp !== undefined ) {
+			return '<span class="citation">' + objectProp + '</span>';			
+		} else {
+			return '';
+		}		
+	}
+
+	document.getElementById('quote-box').innerHTML = '<p class="quote">' + printMakeQuote.quote + '</p>'
+    + '<p class="source">' + printMakeQuote.source + getCitation + getYear + '</p>';
+	
+	
+
+	
+	// !!! These code is commented out because it can create a span tag but could not attach a class into it. !!!
+	// Create a new element and store it in a variable.
+	// var newSpanEl = document.createElement('span');
+	// Create a text node and store it in a variable.
+	// var newCitation = document.createTextNode(printMakeQuote.citation);
+	// Attach the new text node to the new element.
+	// newSpanEl.appendChild(newCitation);
+	// Find the position where the new element should be added.
+	// var position = document.getElementsByClassName('source')[0];
+	// Insert the new element into its position.
+	// position.appendChild(newSpanEl).appendChild('class="citation"');
+
+};
+
+// when user clicks anywhere on the button, the "printQuote" function is called
+ document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
